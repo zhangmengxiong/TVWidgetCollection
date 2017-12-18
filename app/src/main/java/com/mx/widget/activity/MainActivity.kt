f32598a422e3a2c1ab5367c24aff531f280f7f80
ZMX <zmx_final@163.com>
2017-12-18 17:55:58 +0800

    改了几个BUG~

--- app/src/main/java/com/mx/widget/activity/MainActivity.kt
+++ app/src/main/java/com/mx/widget/activity/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : Activity() {
         tvFrameLayoutTxv.setOnClickListener { startActivity(Intent(this, TVFrameLayoutActivity::class.java)) }
         recycleView2FocusTxv.setOnClickListener { startActivity(Intent(this, Recycle2FocusActivity::class.java)) }
 
-        shadeLineBitmap.setRadius(10f)
+        shadeLineBitmap.setRadius(0f)
         shadeLineBitmap.setStroke(50f)
 
         tvHorizontalScrollView.setFocusCenterInViewGroup(true)
